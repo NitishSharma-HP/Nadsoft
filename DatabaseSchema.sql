bc220b3449e070c955e60237f5fc23d26fd5b6e8
Nitish Sharma <106227159+NitishSharma-HP@users.noreply.github.com>
2025-04-18 16:36:56 +0530

    Create DatabaseSchema.sql

---
+++ DatabaseSchema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE marks (
+    id SERIAL PRIMARY KEY,
+    st_id INTEGER NOT NULL,
+    subject VARCHAR(100) NOT NULL,
+    marks INTEGER,
+    FOREIGN KEY (st_id) REFERENCES students(id) ON DELETE CASCADE
+);
+
